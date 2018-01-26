8257639391345d036940ea12e00a36172ad72245
Pujan Thapa <iampujan@outlook.com>
2018-01-26 02:59:31 -0500

    Uploading Image Analysis Code and Data Mining Folder

---
+++ Data Mining/dist_metric.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+
+def minkowski(vec1, vec2, r=2):
+    dist = 0
+    for index, x in enumerate(vec1):
+        dist += abs(vec1[index] - vec2[index]) ** r
+    return dist ** (1/r)
+
+def euclidean(vec1, vec2):
+    return minkowski(vec1, vec2, r=1)
+
+def manhattan(vec1, vec2):
+    return minkowski(vec1, vec2, r=2)
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
